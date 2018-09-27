75915604b3520bb9a4f41e0e16a04f768ee384ab
roy <royli.lihuanan@gmail.com>
2018-09-27 23:38:03 +1000

    Initial Commit

---
+++ ARDicee/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  ARDicee
+//
+//  Created by Roy Li on 27/9/18.
+//  Copyright © 2018 Roy Li. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+
+        // Create Geomotry first
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+        
+        print(ARWorldTrackingConfiguration.isSupported)
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+}
